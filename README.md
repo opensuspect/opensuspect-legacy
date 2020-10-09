6abd9b352c42b33b08e93df1e9cfff135b57c907
Moxvallix <moxvallix@gmail.com>
2020-10-09 18:43:17 +1030

    The First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+OpenSuspect:
+An open source Among Us / Mafia clone.
+
+OpenSuspect aims to be an easily modifiable social deduction game.
+It is built using the Godot engine.
---
+++ source/KinematicBody2D.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
