7ce1deb8e9c69fc7745a087abad94dda8d9a222d
Gabriel Vogel <gabriel.vogel@online.de>
2020-10-11 00:49:35 +0200

    Do not move faster when walking diagonally

--- src/Scripts/KinematicBody2D.gd
+++ src/Scripts/KinematicBody2D.gd
@@ -7,26 +7,29 @@ var id
 var velocity = Vector2(0,0)
 
 func get_input():
-	velocity = Vector2(lerp(velocity.x,0,0.17),lerp(velocity.y,0,0.17))
-	#interpolate velocity:
+	var prev_velocity = velocity
+	velocity = Vector2(0, 0)
 	$Sprite.play("walk")
-	var moving = false
 	if Input.is_action_pressed('ui_right'):
-		velocity.x = speed
+		velocity.x = 1
 		$Sprite.flip_h = false
-		moving = true
 	if Input.is_action_pressed('ui_left'):
-		velocity.x = -speed
+		velocity.x = -1
 		$Sprite.flip_h = true
-		moving = true
 	if Input.is_action_pressed('ui_down'):
-		velocity.y = speed
-		moving = true
+		velocity.y = 1
 	if Input.is_action_pressed('ui_up'):
-		velocity.y = -speed
-		moving = true
-	if not moving:
+		velocity.y = -1
+	velocity = velocity.normalized() * speed
+
+	#interpolate velocity:
+	if velocity.x == 0:
+		velocity.x = lerp(prev_velocity.x, 0, 0.17)
+	if velocity.y == 0:
+		velocity.y = lerp(prev_velocity.y, 0, 0.17)
+	if velocity.length() < 50:
 		$Sprite.play("idle")
+
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
