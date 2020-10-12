a63f3ca24f7922b058203bcb1df3f9ca8eebb675
Ian Flynn <himitsu.monban@gmail.com>
2020-10-12 12:39:17 -0400

    Alphabetize language list

--- src/Scripts/Languages.gd
+++ src/Scripts/Languages.gd
@@ -1,7 +1,13 @@
 extends VBoxContainer
 
+class LanguageSorter:
+	static func sort_ascending(foo, bar) -> bool:
+		return TranslationServer.get_locale_name(foo) < TranslationServer.get_locale_name(bar)
+
 func _ready() -> void:
-	for lang in TranslationServer.get_loaded_locales():
+	var languages: Array = TranslationServer.get_loaded_locales()
+	languages.sort_custom(LanguageSorter, "sort_ascending")
+	for lang in languages:
 		var btn = Button.new()
 		btn.flat = true
 		btn.text = TranslationServer.get_locale_name(lang)
