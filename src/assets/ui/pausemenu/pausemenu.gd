69cf4ca8f5cff2dc8936f4789d081cf0b9252f6a
TheSecondReal0 <asarsprow@gmail.com>
2020-10-20 19:31:42 -0400

    Started on pause menu

---
+++ src/assets/ui/pausemenu/pausemenu.gd
@@ -0,0 +1,16 @@
+extends Popup
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	popup()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
