6dcd9dbc573ad8c3dc2d2b9f523f6229882f29b2
nicemicro <nicemicro@freemail.hu>
2020-10-24 14:04:05 +0900

    Play game / Join game and Create Game scenes: if Enter is pressed while inside the name box, we join the game / create the game.

--- src/assets/ui/mainmenu/playgame/playgame.gd
+++ src/assets/ui/mainmenu/playgame/playgame.gd
@@ -3,16 +3,28 @@ extends Container
 func _ready() -> void:
 	pass
 
-func _on_Connect_pressed() -> void:
+func connect_game() -> void:
 	var hostName: String = $JoinGameMenu/HostnameLine/HostnameField.text
 	var port: int = $JoinGameMenu/Port/PortField.text as int
 	var playerName: String = $JoinGameMenu/Name/NameField.text
 	Network.client(hostName, port, playerName)
 
-func _on_Create_pressed() -> void:
+func create_game() -> void:
 	var port: int = $CreateGameMenu/PortLine/PortField.text as int
 	var playerName: String = $CreateGameMenu/Name/NameField.text
 	Network.client_server(port, playerName)
+
+func _on_Connect_pressed() -> void:
+	connect_game()
+
+func _on_NameField_text_entered_join(new_text):
+	connect_game()
+
+func _on_Create_pressed() -> void:
+	create_game()
+
+func _on_NameField_text_entered_create(new_text):
+	create_game()
 
 func _on_JoinGame_pressed() -> void:
 	show_only('JoinGameMenu')
