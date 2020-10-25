f4c324d2ea360a91f21b982c32aa4085dcf58ffa
TheSecondReal0 <asarsprow@gmail.com>
2020-10-25 19:48:56 -0400

    Misc UI Improvements
    Chat button now reappears when lobby is entered, title no longer jumps around on the title screen

--- src/assets/ui/mainmenu/mainmenu.gd
+++ src/assets/ui/mainmenu/mainmenu.gd
@@ -3,6 +3,7 @@ extends MarginContainer
 signal returnToMainMenu
 
 func _ready() -> void:
+	#get_node("../../../CenterContainer").margin_bottom = 208
 	default_state()
 	print("Loaded locales: ", TranslationServer.get_loaded_locales())
 
@@ -16,6 +17,9 @@ func _on_Return() -> void:
 	default_state()
 
 func default_state() -> void:
+	#print(get_node("../../../CenterContainer").margin_bottom)
+	#get_node("../../../CenterContainer").margin_bottom = 208 #fix title moving to center of screen
+	#print(get_node("../../../CenterContainer").margin_bottom)
 	show_only("MainMenu")
 
 func show_only(element_name: String) -> void:
