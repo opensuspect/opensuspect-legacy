266cfcd662fe58a7179a0389b8bf1e467f0fe2fa
TheSecondReal0 <asarsprow@gmail.com>
2020-10-27 16:42:26 -0400

    Misc

--- src/assets/maps/common/standbutton/standbutton.gd
+++ src/assets/maps/common/standbutton/standbutton.gd
@@ -1,16 +1,21 @@
 extends Area2D
 
 export(NodePath) var interact_path
+export(Dictionary) var interact_info = {}
+export(bool) var only_main_player = false
+export(int, 1, 10000) var players_to_activate = 1
 var overlappingBodies: Array = []
 var pressed: bool = false
 
 func interact():
 	if not get_node(interact_path):
 		return
-	MapManager.interact_with(get_node(interact_path), self)
+	MapManager.interact_with(get_node(interact_path), self, interact_info)
 
 func update():
-	if overlappingBodies.size() < 1:
+	if overlappingBodies.size() < players_to_activate:
+		if not pressed:
+			return
 		pressed = false
 		interact()
 	else:
@@ -25,6 +30,7 @@ func get_state() -> bool:
 func _on_standbutton_body_entered(body):
 	if overlappingBodies.has(body):
 		return
+	if body.get
 	overlappingBodies.append(body)
 	update()
 
