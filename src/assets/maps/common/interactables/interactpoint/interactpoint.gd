d85f117cbf36d36a6c121097276de3554f849543
TheSecondReal0 <asarsprow@gmail.com>
2020-11-02 17:50:19 -0500

    Started interact point

---
+++ src/assets/maps/common/interactables/interactpoint/interactpoint.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+enum type {node, ui}
+export(type) var node_or_ui
+export(NodePath) var node_path
+export(String) var ui_name
+export(Dictionary) var interact_info = {"linkedNode": get_node(node_path)}
+
+func interact():
+	match node_or_ui:
+		type.node:
+			if not get_node(node_path):
+				return
+			MapManager.interact_with(get_node(node_path), self, interact_info)
+		type.ui:
+			UIManager.open_menu(ui_name, interact_info)
