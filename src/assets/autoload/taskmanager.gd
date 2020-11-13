23f59ecd8036efdef49370985a33e995f12f8e83
TheSecondReal0 <asarsprow@gmail.com>
2020-11-13 16:10:49 -0500

    Added TaskManager to autoload

--- src/assets/autoload/taskmanager.gd
+++ src/assets/autoload/taskmanager.gd
@@ -1,16 +1,8 @@
 extends Node
 
+enum task_type {BINARY, ITEM_OUTPUT, ITEM_INPUT}
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var tasks: Array = []
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	print(task_type.BINARY)
