06ebd3ddbfaccc96d62c5743e9c27420feff668d
TheSecondReal0 <asarsprow@gmail.com>
2020-11-22 22:52:33 -0500

    Cleaned up

--- src/addons/opensusinteraction/opensusinteraction.gd
+++ src/addons/opensusinteraction/opensusinteraction.gd
@@ -1,44 +1,30 @@
 tool
 extends EditorPlugin
 
-#inspector plugins
-var interact_inspector_plugin
-var task_inspector_plugin
-
 #custom resources
 var interact_resource_script
 var interactmap_resource_script
 var interactui_resource_script
-var task_resource_script
 
 #icons
 var object_icon
 
 func _enter_tree():
-	#instance inspector plugins
-	task_inspector_plugin = preload("res://addons/opensusinteraction/inspectors/task/taskinspector.gd").new()
-	
 	#load custom resources
 	interact_resource_script = preload("res://addons/opensusinteraction/resources/interact/interact.gd")
 	interactmap_resource_script = preload("res://addons/opensusinteraction/resources/interactmap/interactmap.gd")
 	interactui_resource_script = preload("res://addons/opensusinteraction/resources/interactui/interactui.gd")
-	task_resource_script = preload("res://addons/opensusinteraction/resources/task/task.gd")
 	
 	#load icons
 	object_icon = preload("res://addons/opensusinteraction/icons/object.svg")
-	
-	#add inspector plugins
-	add_inspector_plugin(task_inspector_plugin)
 	
 	#add custom resources
 	add_custom_type("Interact", "Resource", interact_resource_script, object_icon)
 	add_custom_type("InteractMap", "Resource", interactmap_resource_script, object_icon)
 	add_custom_type("InteractUI", "Resource", interactui_resource_script, object_icon)
-	add_custom_type("Task", "Resource", task_resource_script, object_icon)
 
 
 func _exit_tree():
 	remove_custom_type("Interact")
 	remove_custom_type("InteractMap")
 	remove_custom_type("InteractUI")
-	remove_custom_type("Task")
--- src/addons/opensusinteraction/resources/interact/interact.gd
+++ src/addons/opensusinteraction/resources/interact/interact.gd
@@ -3,16 +3,14 @@ extends Resource
 
 class_name Interact
 
-enum type {task = 0, ui = 1, map = 2}
+enum type {ui = 1, map = 2}
 export(type) var interact_type
 
 #needed to instance new unique resources in editor
-var base_task_resource: Resource = ResourceLoader.load("res://addons/opensusinteraction/resources/task/task.tres")
 var base_ui_resource: Resource = ResourceLoader.load("res://addons/opensusinteraction/resources/interactui/interactui.tres")
 var base_map_resource:Resource = ResourceLoader.load("res://addons/opensusinteraction/resources/interactmap/interactmap.tres")
 
 #changed in the editor via overriding get(), set(), and get_property_list()
-export var task_resource: Resource = base_task_resource.duplicate()
 export var ui_resource: Resource = base_ui_resource.duplicate()
 export var map_resource: Resource = base_map_resource.duplicate()
 
@@ -21,9 +19,6 @@ var interact_data: Dictionary = {}
 #called to execute the interaction this resource is customized for
 func interact(_from: Node):
 	match interact_type:
-		type.task:
-			pass
-			#task_resource.interact(_from)
 		type.ui:
 			ui_resource.interact(_from)
 		type.map:
@@ -33,9 +28,6 @@ func get_interact_data(_from: Node = null) -> Dictionary:
 	var interact_data: Dictionary = {}
 	var res_interact_data: Dictionary = {}
 	match interact_type:
-		type.task:
-			pass
-			#res_interact_data = task_resource.get_interact_data()
 		type.ui:
 			res_interact_data = ui_resource.get_interact_data(_from)
 		type.map:
@@ -56,38 +48,10 @@ func _init():
 #overrides set(), allows for export var groups and display properties that don't
 #match actual var names
 func _set(property, value):
-#	#add custom stuff to inspector and use this to figure out what it's trying to do
+#	#add custom stuff to inspector and use this to see what it's trying to do
 #	#so you can figure out how to handle it
 #	#print("setting ", property, " to ", value)
 	match property:
-#		"task":
-#			#if new resource is a task resource
-#			if value is preload("res://addons/opensusinteraction/resources/task/task.gd"):
-#				task_resource = value
-#			else:
-#				#create new task resource
-#				task_resource = base_task_resource.duplicate()
-#		"ui":
-#			#if new resource is a ui interact resource
-#			if value is preload("res://addons/opensusinteraction/resources/interactui/interactui.gd"):
-#				ui_resource = value
-#			else:
-#				#create new ui interact resource
-#				ui_resource = base_ui_resource.duplicate()
-#		"map":
-#			#if new resource is a map interact resource
-#			if value is preload("res://addons/opensusinteraction/resources/interactmap/interactmap.gd"):
-#				map_resource = value
-#			else:
-#				#create new map interact resource
-#				map_resource = base_map_resource.duplicate()
-		"task_resource":
-			#if new resource is a task resource
-			if value is preload("res://addons/opensusinteraction/resources/task/task.gd"):
-				task_resource = value
-			else:
-				#create new task resource
-				task_resource = base_task_resource.duplicate()
 		"ui_resource":
 			#if new resource is a ui interact resource
 			if value is preload("res://addons/opensusinteraction/resources/interactui/interactui.gd"):
@@ -103,39 +67,11 @@ func _set(property, value):
 				#create new map interact resource
 				map_resource = base_map_resource.duplicate()
 #	property_list_changed_notify()
-#	return true
-
-#overrides get(), allows for export var groups and display properties that don't
-#match actual var names
-#func _get(property):
-#	if not Engine.editor_hint:
-#		return []
-#	match property:
-#		"task":
-#			return task_resource
-#		"ui":
-#			return ui_resource
-#		"map":
-#			return map_resource
+	return true
 
 #overrides get_property_list(), tells editor to show more vars in inspector
-func _get_property_list():
-	#if not Engine.editor_hint:
-	#	return []
-	var property_list: Array = []
-#	property_list.append({"name": "task",
-#		"type": TYPE_OBJECT,
-#		"usage": PROPERTY_USAGE_DEFAULT,
-#		"hint": PROPERTY_HINT_RESOURCE_TYPE,
-#		})
-#	property_list.append({"name": "ui",
-#				"type": TYPE_OBJECT,
-#				"usage": PROPERTY_USAGE_DEFAULT,
-#				"hint": PROPERTY_HINT_RESOURCE_TYPE,
-#				})
-#	property_list.append({"name": "map",
-#				"type": TYPE_OBJECT,
-#				"usage": PROPERTY_USAGE_DEFAULT,
-#				"hint": PROPERTY_HINT_RESOURCE_TYPE,
-#				})
-	return property_list
+#func _get_property_list():
+#	#if not Engine.editor_hint:
+#	#	return []
+#	var property_list: Array = []
+#	return property_list
