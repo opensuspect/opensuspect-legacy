16098c5fc717ca1cde061ff071243b69ed0f652c
TheRyeGuyWhoWillNowDie <logangilbertson@linuxmail.org>
2020-11-23 19:06:48 -0600

    map is downloaded from server to client, will add url map downloading later

--- src/assets/main/maps.gd
+++ src/assets/main/maps.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var maps: Dictionary = {"lobby": {"dir": preload("res://assets/maps/lobby/lobby.tscn")}, "test": {"dir": preload("res://assets/maps/test/test.tscn")}}
+var maps: Dictionary = {"lobby": {"dir": preload("res://assets/maps/lobby/lobby.tscn")}, "test": {"dir": load("user://maps/currentmap.tscn")}}
 
 signal spawn(position,frommap)
 
