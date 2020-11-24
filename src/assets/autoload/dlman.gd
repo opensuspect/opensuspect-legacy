48e64909ea1751f35b1610948ff28d9cdbdd7450
TheRyeGuyWhoWillNowDie <logangilbertson@linuxmail.org>
2020-11-23 20:31:20 -0600

    when custom map cannot be loaded, it defaults to test.tscn

--- src/assets/autoload/dlman.gd
+++ src/assets/autoload/dlman.gd
@@ -5,13 +5,16 @@ extends Node
 # var a = 2
 # var b = "text"
 var mapdata
-
+var http
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_network_master(1)
 func distribute():
 	var map = File.new()
 	map.open("user://maps/servermap.tscn", File.READ)
+	if not map.file_exists("user://maps/servermap.tscn"):
+		map.close()
+		map.open("res://assets/maps/test/test.tscn", File.READ)
 	mapdata = map.get_as_text()
 	rpc("recieve",mapdata)
 	recieve(mapdata)
