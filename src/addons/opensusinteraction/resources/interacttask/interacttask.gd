971c7d34e3a1b165c073ac838d69d58b63cb297a
TheSecondReal0 <asarsprow@gmail.com>
2020-11-26 18:32:39 -0500

    Improving task resource
    * Now uses InteractUI for the task UI
    * Output tasks are now references to nodes which should have tasks attached

--- src/addons/opensusinteraction/resources/interacttask/interacttask.gd
+++ src/addons/opensusinteraction/resources/interacttask/interacttask.gd
@@ -1,11 +1,11 @@
-#tool
+tool
 extends Resource
 
 #class_name InteractTask
 
 export(String) var task_name
 
-export(String) var ui_name
+#export(Resource) var ui_name = base_ui_resource.duplicate()
 
 var item_inputs_on: bool
 var item_inputs: PoolStringArray
@@ -13,14 +13,18 @@ var item_inputs: PoolStringArray
 var item_outputs_on: bool
 var item_outputs: PoolStringArray
 
-#needed to instance new unique task resources in editor
-var base_task_resource: Resource = ResourceLoader.load("res://addons/opensusinteraction/resources/task/task.tres")
+var map_outputs_on: bool
+var map_outputs: Array
 
 var task_outputs_on: bool
 var task_outputs: Array
 
-var list_abc = true
-var abc = "InteractTaskScript"
+#needed to instance new unique resources in editor
+var base_ui_resource: Resource = ResourceLoader.load("res://addons/opensusinteraction/resources/interactui/interactui.tres")
+var base_map_resource:Resource = ResourceLoader.load("res://addons/opensusinteraction/resources/interactmap/interactmap.tres")
+
+#changed in the editor via overriding get(), set(), and get_property_list()
+var ui_res: Resource = base_ui_resource.duplicate()
 
 func init_task():
 	TaskManager.add_task_resource(self)
@@ -32,7 +36,7 @@ func get_task_info() -> Dictionary:
 func gen_task_info() -> Dictionary:
 	var info:Dictionary = {}
 	info["task_name"] = task_name
-	info["ui_name"] = ui_name
+#	info["ui_name"] = ui_name
 	info["item_inputs"] = item_inputs
 	info["item_outputs"] = item_outputs
 	info["task_outputs"] = task_outputs
@@ -49,6 +53,14 @@ func _init():
 #match actual var names
 func _set(property, value):
 	match property:
+		"ui_resource":
+			#if new resource is a ui interact resource
+			if value is preload("res://addons/opensusinteraction/resources/interactui/interactui.gd"):
+				ui_res = value
+			else:
+				#create new ui interact resource
+				ui_res = base_ui_resource.duplicate()
+
 		"inputs/toggle_items":
 			item_inputs_on = value
 			property_list_changed_notify()
@@ -63,6 +75,17 @@ func _set(property, value):
 		"outputs/output_items":
 			item_outputs = value
 
+		"outputs/toggle_map_interactions":
+			map_outputs_on = value
+			property_list_changed_notify()
+
+		"outputs/output_map_interactions":
+			map_outputs = value
+			if map_outputs.size() > 0 and map_outputs[-1] == null:
+				#print(map_outputs)
+				map_outputs[-1] = base_map_resource.duplicate()
+			property_list_changed_notify()
+
 		"outputs/toggle_tasks":
 			task_outputs_on = value
 			property_list_changed_notify()
@@ -70,8 +93,8 @@ func _set(property, value):
 		"outputs/output_tasks":
 			task_outputs = value
 			if task_outputs.size() > 0 and task_outputs[-1] == null:
-				print("null last task")
-				task_outputs[-1] = base_task_resource.duplicate()
+				#print(task_outputs)
+				task_outputs[-1] = NodePath("")#base_task_resource.duplicate()
 			property_list_changed_notify()
 	return true
 
@@ -79,6 +102,9 @@ func _set(property, value):
 #match actual var names
 func _get(property):
 	match property:
+		"ui_resource":
+			return ui_res
+
 		"inputs/toggle_items":
 			return item_inputs_on
 		"inputs/input_items":
@@ -88,6 +114,11 @@ func _get(property):
 			return item_outputs_on
 		"outputs/output_items":
 			return item_outputs
+
+		"outputs/toggle_map_interactions":
+			return map_outputs_on
+		"outputs/output_map_interactions":
+			return map_outputs
 
 		"outputs/toggle_tasks":
 			return task_outputs_on
@@ -99,6 +130,12 @@ func _get_property_list():
 #	if not Engine.editor_hint:
 #		return []
 	var property_list: Array = []
+
+	property_list.append({"name": "ui_resource",
+		"type": TYPE_OBJECT,
+		"usage": PROPERTY_USAGE_DEFAULT,
+		"hint": PROPERTY_HINT_RESOURCE_TYPE,
+		})
 
 	#item input toggle
 	property_list.append({
@@ -132,6 +169,22 @@ func _get_property_list():
 		"hint": PROPERTY_HINT_NONE,
 		})
 
+	#item output toggle
+	property_list.append({
+		"name": "outputs/toggle_map_interactions",
+		"type": TYPE_BOOL,
+		"usage": PROPERTY_USAGE_DEFAULT,
+		"hint": PROPERTY_HINT_NONE,
+		})
+	#item output array field
+	if map_outputs_on:
+		property_list.append({
+		"name": "outputs/output_map_interactions",
+		"type": TYPE_ARRAY,
+		"usage": PROPERTY_USAGE_DEFAULT,
+		"hint": PROPERTY_HINT_NONE,
+		})
+
 	#task output toggle
 	property_list.append({
 		"name": "outputs/toggle_tasks",
@@ -144,7 +197,7 @@ func _get_property_list():
 		"name": "outputs/output_tasks",
 		"type": TYPE_ARRAY,
 		"usage": PROPERTY_USAGE_DEFAULT,
-		"hint": PROPERTY_HINT_NONE,
+		"hint": PROPERTY_HINT_DIR,
 		"hint_string": ""
 		})
 	return property_list
