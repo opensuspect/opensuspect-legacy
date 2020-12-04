7da8d5b80efcb0bb6b4a27f46105b66edae91d75
TheSecondReal0 <asarsprow@gmail.com>
2020-12-04 00:23:34 -0500

    Misc

--- src/assets/ui/lobbyui/votemenu/votemenu.gd
+++ src/assets/ui/lobbyui/votemenu/votemenu.gd
@@ -4,14 +4,9 @@ onready var button_grid: Node = get_node("TabContainer/Vote/VoteControl/VBoxCont
 
 func _ready():
 	show()
+	# hides x so players can't close the menu
 	get_close_button().hide()
 	button_grid.connect("vote", self, "vote_for")
 
 func vote_for(player_id):
 	print("voting for ", player_id)
-
-#func _on_visibility_changed():
-#	if visible:
-#		UIManager.ui_opened("votemenu")
-#	else:
-#		UIManager.ui_closed("votemenu")
