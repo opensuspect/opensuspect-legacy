3d84c7834477458a4214c3774c8022513f9156a9
TheSecondReal0 <asarsprow@gmail.com>
2020-12-08 11:27:55 -0500

    Added state_changed_priority and implemented

--- src/assets/autoload/gamemanager.gd
+++ src/assets/autoload/gamemanager.gd
@@ -9,9 +9,12 @@ const TRANSITIONS = {
 	State.Normal: [State.Lobby, State.Start],
 }
 
+var priority_amount: int = 6
+
 #signals that help sync the gamestate
 #can be connected to from anywhere with GameManager.connect("<signal name>", self, "<function name>")
-signal state_changed
+signal state_changed(old_state, new_state)
+signal state_changed_priority(old_state, new_state, priority)
 
 func _ready():
 	set_network_master(1)
@@ -20,31 +23,37 @@ func _ready():
 # warning-ignore:return_value_discarded
 	Network.connect("server_started", self, "_on_connected")
 
-func transition(new_state) -> bool:
+func transition(new_state: int) -> bool:
 	print("attempting to transition gamestate from ", state, " to ", new_state)
 	if (TRANSITIONS[state].has(new_state)):
 		var old_state: int = state
 		state = new_state
 		if get_tree().is_network_server():
-			rpc("receiveTransition", new_state)
-		emit_signal('state_changed', old_state, new_state)
+			rpc("receive_transition", new_state)
+		emit_state_changed_signals(old_state, new_state)
 		print("transition successful")
 		return true
 	print("transition failed")
 	return false
 
-puppet func receiveTransition(new_state):
+puppet func receive_transition(new_state: int):
 	if get_tree().is_network_server():
 		return
 	print("attempting to transition gamestate from ", state, " to ", new_state)
 	if (TRANSITIONS[state].has(new_state)):
 		var old_state: int = state
 		state = new_state
-		emit_signal('state_changed', old_state, new_state)
+		emit_state_changed_signals(old_state, new_state)
 		print("transition successful")
 		return# true
 	print("transition failed")
 	return# false
+
+func emit_state_changed_signals(old_state: int, new_state: int):
+	# emit state_changed_priority, priority starts at 0
+	for priority in priority_amount:
+		emit_signal("state_changed_priority", old_state, new_state, priority)
+	emit_signal('state_changed', old_state, new_state)
 
 func get_state() -> int:
 	return state
