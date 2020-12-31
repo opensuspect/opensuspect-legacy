5df305b84872451485122c66adb3e5d46bd46bda
cukmekerb <cukmekerb+git@gmail.com>
2020-12-30 20:53:47 -0800

    updated variable name; wasted time trying to not break stuff

--- src/assets/player/infiltrator.gd
+++ src/assets/player/infiltrator.gd
@@ -32,10 +32,9 @@ var _target_player: KinematicBody2D
 func _ready() -> void:
 	if player.main_player:
 		_instantiate_kill_gui()
-	# Get Main parent node and connect kill player signal to it
-	var main: YSort = get_tree().get_root().get_node("Main").get_node("players")
-	print(main)
-	connect("kill", main, "_on_infiltrator_kill")
+	# Get players.gd node and connect kill player signal to it
+	var player_node: YSort = get_tree().get_root().get_node("Main").get_node("players")
+	connect("kill", player_node, "_on_infiltrator_kill")
 
 func _process(delta: float) -> void:
 	if player.main_player:
