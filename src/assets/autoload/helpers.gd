00402212317194b6dd918f3a3868fb71702d053b
TheSecondReal0 <asarsprow@gmail.com>
2020-12-31 00:45:17 -0500

    Added MapInfo resources
    * Added MapInfo resource class
    * Created MapInfo resources for existing maps (lobby and test)
    * Created helpers to find certain types of files in a directory

--- src/assets/autoload/helpers.gd
+++ src/assets/autoload/helpers.gd
@@ -28,6 +28,56 @@ func list_directory(path: String, recursive: bool = false) -> Array:
 		file_name = directory.get_next()
 	return files
 
+func load_files_in_dir_with_exts(directory: String, exts: PoolStringArray) -> Array:
+	var paths: Array = get_file_paths_in_dir_with_exts(directory, exts)
+	var resources: Array = []
+	for path in paths:
+		var res: Resource = load(path)
+		resources.append(res)
+	return resources
+
+func get_file_paths_in_dir_with_exts(directory: String, exts: PoolStringArray) -> Array:
+	var paths: Array = []
+	for ext in exts:
+		paths += get_file_paths_in_dir(directory, ext)
+	return paths
+
+func get_file_paths_in_dir(directory: String, ext: String = "") -> Array:
+	var paths: Array = []
+	var dir = Directory.new()
+	dir.open(directory)
+	dir.list_dir_begin()
+	while true:
+		var file = dir.get_next()
+		if file == "":
+			# completely break out of the loop
+			break
+		# if file path doesn't have the right extension
+		if not is_valid_file_name(file, ext):
+			# skip to the next iteration of the loop
+			continue
+		var path: String = directory
+		if not path.ends_with("/"):
+			path += "/"
+		path += file
+		paths.append(path)
+	dir.list_dir_end()
+	return paths
+
+func is_valid_file_name(file_name: String, ext: String = "") -> bool:
+	if file_name == "":
+		return false
+	# if file path doesn't have the right extension
+	if ext != "" and not file_name.ends_with(ext):
+		return false
+		# don't include non-files
+	if file_name == "." or file_name == "..":
+		return false
+	# if file is actually a folder
+	if file_name.split(".").size() < 2:
+		return false
+	return true
+
 func map(in_value: float, in_value_min: float, in_value_max: float, out_value_min: float, out_value_max: float) -> float:
 	return (in_value - in_value_min) * (out_value_max - out_value_min) / (in_value_max - in_value_min) + out_value_min
 
--- src/assets/main/maps.gd
+++ src/assets/main/maps.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
-var maps: Dictionary = {"lobby": {"dir": preload("res://assets/maps/lobby/lobby.tscn")}, "test": {"dir": preload("res://assets/maps/test/test.tscn")}}
+#var maps: Dictionary = {"lobby": {"dir": preload("res://assets/maps/lobby/lobby.tscn")}, "test": {"dir": preload("res://assets/maps/test/test.tscn")}}
+var map_info: Dictionary = {}
+
+var map_info_dir: String = "res://assets/maps/mapinfo/"
+#const MapInfo = preload("res://assets/maps/mapinfo/mapinforesource/mapinfo.gd")
 
 signal spawn(position,frommap)
 
@@ -9,6 +13,10 @@ var currentMap: String = "lobby"
 func _ready() -> void:
 	set_network_master(1)
 	switchMap(currentMap)
+#	print(load_map_info_resources())
+#	print(Helpers.get_file_paths_in_dir(map_info_dir))
+#	print(gen_map_info())
+	update_map_info()
 # warning-ignore:return_value_discarded
 	GameManager.connect("state_changed_priority", self, "_on_state_changed_priority")
 
@@ -18,21 +26,33 @@ func _on_state_changed_priority(old_state: int, new_state: int, priority: int) -
 		return
 	match new_state:
 		GameManager.State.Lobby:
-			switchMap('lobby')
+			switchMap("Lobby")
 		GameManager.State.Normal:
-			switchMap('test')
+			switchMap("Test")
 
 func switchMap(newMap: String) -> void:
 	print('switchMap called for ', newMap)
-	if not maps.keys().has(newMap):
+	print("map_info: ", map_info)
+	if map_info.empty():
+		update_map_info()
+	print("map_info: ", map_info)
+	if not map_info.keys().has(newMap):
+		push_error("Attempting to switch to a map that does not exist, the resource could be missing or it was parsed incorrectly.")
 		return
 	print("loading map: ", newMap)
 	for i in get_children():
 		i.queue_free()
 	currentMap = newMap
-	var mapClone = maps[newMap].dir.instance()
+	var mapClone = instance_map(newMap)
 	add_child(mapClone)
 	emit_signal("spawn", getSpawnPoints())
+
+func instance_map(map_name: String) -> Node:
+	var map_path: String = map_info[map_name]["scene_path"]
+	print(map_path)
+	var map_scene: PackedScene = load(map_path)
+	var map: Node = map_scene.instance()
+	return map
 
 func getSpawnPoints() -> Array:
 	var spawnPointArray: Array = []
@@ -42,3 +62,26 @@ func getSpawnPoints() -> Array:
 		spawnPointArray.append(i.global_position)
 	#print(spawnPointArray)
 	return spawnPointArray
+
+func update_map_info() -> void:
+	map_info = gen_map_info()
+
+func gen_map_info() -> Dictionary:
+	var resources: Array = load_map_info_resources()
+	var new_map_info: Dictionary = {}
+	for res in resources:
+		var dict: Dictionary = {}
+		var res_name: String = res.get_name()
+		# including name inside nested dict to make it easier if we want for loops later
+		for property in ["name", "desc", "thumbnail", "scene_path"]:
+			dict[property] = res.get(property)
+		dict["res"] = res
+		new_map_info[res_name] = dict
+	return new_map_info
+
+func load_map_info_resources() -> Array:
+	var resources: Array = Helpers.load_files_in_dir_with_exts(map_info_dir, [".tres", ".res"])
+	for res in resources:
+		if not res is MapInfo:
+			resources.erase(res)
+	return resources
