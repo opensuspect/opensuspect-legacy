5233618b9e9e58287b88fda8a4d721b486d3c35f
Zapzoop0099 <zapzoop0099@gmail.com>
2021-03-28 11:50:31 +0530

    Basic scene setup

--- src/assets/ui/tasks/container/container.gd
+++ src/assets/ui/tasks/container/container.gd
@@ -5,11 +5,9 @@ onready var grid = $background/conatainergrid
 const slot_scene = preload("res://assets/ui/tasks/container/itemslot.tscn")
 
 var available_items:Dictionary = {
-							"battery":{"scene":preload("res://assets/items/battery.tscn")},
-							"wrench":{"scene":preload("res://assets/items/wrench.tscn")},
-							"large-liquid-bottle":{"scene":preload("res://assets/items/large-liquid-bottle.tscn")},
-							"powder-bottle":{"scene":preload("res://assets/items/powder-bottle.tscn")},
-							"small-liquid-bottle":{"scene":preload("res://assets/items/small-liquid-bottle.tscn")}
+							"large-liquid-bottle":{"scene":preload("res://assets/items/large-liquid-bottle.tscn"),"position":Vector2(40,56),"shift":8,"scale":Vector2(1.5,1.5)},
+							"powder-bottle":{"scene":preload("res://assets/items/powder-bottle.tscn"),"position":Vector2(40,80),"shift":8,"scale":Vector2(2,2)},
+							"small-liquid-bottle":{"scene":preload("res://assets/items/small-liquid-bottle.tscn"),"position":Vector2(40,64),"shift":8,"scale":Vector2(2,2)}
 }
 var current_item_instanced:Array = []
 var slots_in_grid:Dictionary={}
@@ -31,17 +29,19 @@ func _on_closebutton_pressed():#Resets the data which is passed
 	interact({},get_res().actions.CLOSE)
 
 func _on_set_scene() -> void:
+	print("called")
 	#Place where the scene is built
 	for num in range(0, slots):
 		var slot = slot_scene.instance()
 		slot.index = num
 		slot.container = self
-		grid.add_child(slot)
-		var item_to_instance = place_item(slot)
-		if item_to_instance == null:
-			break
-		var number_of_item = set_amount(slot)
-		slots_in_grid[slot.index] = {"slot":slot,"item_instanced":item_to_instance,"number_of_item":number_of_item}
+		if can_set_up_items(slot):
+			grid.add_child(slot)
+#		var item_to_instance = place_item(slot)
+#		if item_to_instance == null:
+#			break
+#		var number_of_item = set_amount(slot)
+#		slots_in_grid[slot.index] = {"slot":slot,"item_instanced":item_to_instance,"number_of_item":number_of_item}
 	get_res().slots_info = slots_in_grid
 
 func _on_erase_children() -> void:#erases all child
@@ -69,27 +69,68 @@ func update():
 	get_res().connect("set_scene", self, "_on_set_scene")
 	get_res().connect("erase_children", self, "_on_erase_children")
 
-func randomizer(item):
-	for items in available_items.keys():
-		if item == items:
-			var item_as_child = available_items[item].scene.instance()
-			return item_as_child
+#func randomizer(item,number_of_item):
+#	var item_as_child 
+#	for items in available_items.keys():
+#		if item == items:
+#			for item in range(0, number_of_item):
+#				item_as_child = available_items[item].scene.instance()
+#			return item_as_child
 
-func set_position_and_add_child(slot, item):
-	item.position += Vector2(64,64)
-	slot.add_child(item)
+#func set_position_and_add_child(slot, item):
+#	item.position += Vector2(64,64)
+#	slot.add_child(item)
 
 
 func set_amount(slot):
 	var number_of_item = Helpers.pick_random(range(0,11))
-	slot.amount.text = str(number_of_item)
 	return number_of_item
 
-func place_item(slot):
+func random_item(slot):
 	var item_to_instance = Helpers.pick_random(available_items.keys())
 	if current_item_instanced.has(item_to_instance):
 		return null
 	else:
 		current_item_instanced.append(item_to_instance)
-		set_position_and_add_child(slot, randomizer(item_to_instance))
 		return item_to_instance
+
+func can_set_up_items(slot) -> bool:
+	var item_as_child
+	var item_to_instanced = random_item(slot)
+	if item_to_instanced == null:
+		return false
+	print(item_to_instanced)
+	var number_of_item = set_amount(slot)
+	
+	if not available_items.keys().has(item_to_instanced):
+		return false
+	
+	for item_count in range(0,number_of_item):
+		item_as_child = available_items[item_to_instanced].scene.instance()
+		slot.add_child(item_as_child)
+		slot.move_child(item_as_child,0)
+		set_item_position(item_to_instanced,item_as_child,item_count,number_of_item)
+		slots_in_grid[slot.index] = {"slot":slot,"item_instanced":item_to_instanced,"number_of_item":number_of_item}
+	return true
+	#set_position_and_add_child(slot, randomizer(item_to_instanced,number_of_item))
+	
+func set_item_position(item_name,item,item_count,number_of_item):
+	var item_scale = available_items[item_name].scale
+	item.set_scale(item_scale)
+	item.position += available_items[item_name].position
+	if not item_count != 0:
+		return
+	var item_shift = available_items[item_name].shift
+	var position = item.position
+	position.x += item_count * item_shift
+	position.y -= item_count * item_shift
+	item.position = position
+		#var item_shift = available_items[item_name].shift
+		#print(item.position)
+		#var position = item.position
+		#position.x += item_shift
+		#position.y -= item_shift
+		#print(position)
+		#item.position = position 
+	
+	
--- src/assets/ui/tasks/container/itemslot.gd
+++ src/assets/ui/tasks/container/itemslot.gd
@@ -7,32 +7,29 @@ var mouse_entered:bool
 var index:int
 var container
 
-signal input_received(ui_data, index)
-
 func _ready():
 	pass
 	
 
 func _on_Control_gui_input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT and mouse_entered == true:
-		if itemslot.get_child(1) != null:
+		if itemslot.get_child(0) != null:
 			container.get_res().generate_interact_data(index)
 
 
-func _on_Control_mouse_entered():
-	if itemslot.get_child_count() == 1:
+func _on_itemslot_mouse_entered():
+	if itemslot.get_child_count() == 0:
 		return
 	else:
-		itemslot.get_child(1).animator.play("hover")
-		itemslot.get_child(1).can_pickup_with_mouse = true
+		itemslot.get_child(get_child_count() - 1).animator.play("hover")
+		itemslot.get_child(get_child_count() - 1).can_pickup_with_mouse = true
 		mouse_entered = true
 
 
-func _on_Control_mouse_exited():
-	if itemslot.get_child_count() == 1:
+func _on_itemslot_mouse_exited():
+	if itemslot.get_child_count() == 0:
 		return
 	else:
-		itemslot.get_child(1).animator.play("idle")
-		itemslot.get_child(1).can_pickup_with_mouse = false
+		itemslot.get_child(get_child_count() - 1).animator.play("idle")
+		itemslot.get_child(get_child_count() - 1).can_pickup_with_mouse = false
 		mouse_entered = false
-
